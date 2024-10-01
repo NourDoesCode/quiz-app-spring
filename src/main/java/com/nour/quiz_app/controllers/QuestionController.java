e2cd9403120f7d03f4c8fe166352ce0798c90cde
nour <174273417+NourDoesCode@users.noreply.github.com>
2024-10-01 22:55:07 +0100

    first commit

---
+++ src/main/java/com/nour/quiz_app/controllers/QuestionController.java
@@ -0,0 +1,25 @@
+package com.nour.quiz_app.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nour.quiz_app.models.Question;
+import com.nour.quiz_app.services.QuestionService;
+
+@RestController
+// domain.com/question
+@RequestMapping("question")
+public class QuestionController {
+    @Autowired
+    QuestionService questionService;
+
+    @GetMapping("allQuestions")
+    public List<Question> getAllQuestions() {
+        return questionService.getAllQuestions();
+    }
+
+}
---
+++ src/main/java/com/nour/quiz_app/services/QuestionService.java
@@ -0,0 +1,17 @@
+package com.nour.quiz_app.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.nour.quiz_app.models.Question;
+
+@Service
+public class QuestionService {
+
+    public List<Question> getAllQuestions() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAllQuestions'");
+    }
+
+}
